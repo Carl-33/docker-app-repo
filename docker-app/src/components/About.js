aeb1f07d2886588b188c88752b03b49deedefe90
llammers <llammers017@gmail.com>
2021-12-21 14:19:50 -0600

    ducks on docks

--- docker-app/src/components/About.js
+++ docker-app/src/components/About.js
@@ -2,7 +2,13 @@ function About() {
     return (
         <div className="text-center">
             <h1> About </h1>
-            <img className="card-img-top-small" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYr6Ar37yaH7JgepXUsN9Tq1XaGZW9-f5K5A&usqp=CAU" />
+            <figure>
+                <img className="card-img-top-small" src="https://media.istockphoto.com/photos/duck-family-resting-on-a-dock-picture-id1146483681" />
+                <figurecaption>
+                    <h4>You, too, could be this happy!</h4>
+                    <p className="fine-print">(Ducks not included)</p>
+                </figurecaption>
+            </figure>
 
             <p >As one of the top dock sellers, Dock Dock Gray Dock provides a versatile and diverse product line for all of our clients. From residential to commercial use, we offer docks appropriate for all needs. Our docks are durable, eco-friendly, and tested to be of the best quality. We offer fantastic quality for an unbeatable price.</p>
             <p className="fine-print">(Must have at least $15,000 to do business)</p>
